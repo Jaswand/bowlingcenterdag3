bb0108b1482af1d29af7098d2c9de542b6d99743
Jaswand Soekhoe <jp.soekhoe@outlook.com>
2025-01-30 14:32:06 +0100

    update index

--- resources/views/reserveringoverzicht/index.blade.php
+++ resources/views/reserveringoverzicht/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+    {{-- Page Header --}}
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left"></div>
@@ -9,39 +10,48 @@
         </div>
     </div>
 
+    {{-- Filter Form Section --}}
     <div class="row mb-3">
         <div class="col-lg-4 offset-lg-8">
+            {{-- Form for filtering reservations by date --}}
             <form action="{{ route('reserveringoverzicht.filter') }}" method="GET" class="form-inline">
                 <div class="form-group">
                     <label for="datum" class="mr-2">Selecteer datum:</label>
                     <select name="datum" id="datum" class="form-control mr-2">
+                        {{-- Default option --}}
                         <option value="">Selecteer datum</option>
+                        {{-- Populate options dynamically from the reservation data --}}
                         @if($reservering && is_iterable($reservering))
                             @foreach ($reservering as $item)
                                 <option value="{{ $item->datum }}">{{ $item->datum }}</option>
                             @endforeach
                         @endif
                     </select>
+                    {{-- Submit button to apply the filter --}}
                     <button type="submit" class="btn btn-primary">Tonen</button>
                 </div>
             </form>
         </div>
     </div>
 
+    {{-- Success Message --}}
     @if ($message = Session::get('success'))
         <div class="alert alert-success">
             <p>{{ $message }}</p>
         </div>
     @endif
 
+    {{-- Error Message --}}
     @if ($message = Session::get('error'))
         <div class="alert alert-danger">
             <p>{{ $message }}</p>
         </div>
     @endif
 
+    {{-- Reservation Table --}}
     <table class="table table-striped">
         <tr>
+            {{-- Table headers for reservation details --}}
             <th>Voornaam</th>
             <th>Tussenvoegsel</th>
             <th>Achternaam</th>
@@ -51,38 +61,52 @@
             <th>Kinderen</th>
             <th>Status</th>
         </tr>
+
+        {{-- Check if there are any reservations to display --}}
         @if($reservering && is_iterable($reservering))
+            {{-- Loop through each reservation and display details --}}
             @foreach ($reservering as $item)
             <tr>
+                {{-- Display personal details with fallback to 'N/A' if data is missing --}}
                 <td>{{ $item->persoon->voornaam ?? 'N/A' }}</td>
                 <td>{{ $item->persoon->tussenvoegsel ?? 'N/A' }}</td>
                 <td>{{ $item->persoon->achternaam ?? 'N/A' }}</td>
+
+                {{-- Display reservation details --}}
                 <td>{{ $item->datum ?? 'N/A' }}</td>
-                <td>{{ $item->uren ?? 'N/A' }}</td>
-                <td>{{ $item->aantalvolwassen ?? 'N/A' }}</td>  
-                <td>{{ $item->aantalkinderen ?? 'N/A' }}</td> 
+                <td>{{ $item->aantaluren ?? 'N/A' }}</td>
+                <td>{{ $item->aantalvolwassen ?? 'N/A' }}</td>
+                <td>{{ $item->aantalkinderen ?? 'N/A' }}</td>
+
+                {{-- Display reservation status with color-coded badges --}}
                 <td>
                     @if($item->status_id)
                         @switch($item->status->id)
-                        @case(1)
-                        <span class="badge bg-success text-dark">{{ $item->status->name }}</span>    
-                        @break
-                        @case(2)
-                        <span class="badge bg-warning text-dark">{{ $item->status->name }}</span>
-                        @break
-                        @case(3)
-                        <span class="badge bg-danger text-dark">{{ $item->status->name }}</span>
-                        @break
-                        @default
-                        <span class="badge bg-secondary text-dark">{{ $item->status->name }}</span>
+                            @case(1)
+                                {{-- Green badge for status 1 --}}
+                                <span class="badge bg-success text-dark">{{ $item->status->naam }}</span>    
+                                @break
+                            @case(2)
+                                {{-- Yellow badge for status 2 --}}
+                                <span class="badge bg-warning text-dark">{{ $item->status->naam }}</span>
+                                @break
+                            @case(3)
+                                {{-- Red badge for status 3 --}}
+                                <span class="badge bg-danger text-dark">{{ $item->status->naam }}</span>
+                                @break
+                            @default
+                                {{-- Default grey badge for other statuses --}}
+                                <span class="badge bg-secondary text-dark">{{ $item->status->naam }}</span>
                         @endswitch
                     @else
+                        {{-- Display N/A if no status is set --}}
                         <span class="badge bg-secondary text-dark">N/A</span>
                     @endif   
                 </td>                 
             </tr>
             @endforeach
         @else
+            {{-- Display a message if no reservations are found --}}
             <tr>
                 <td colspan="8">Geen reserveringen gevonden.</td>
             </tr>
